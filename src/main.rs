7c94c0a5dcf13d7de06d9508cfca2424bd114b4d
gAkira <guilherme.akira11@gmail.com>
2022-11-14 23:22:50 +0100

    Add comments along all the code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+// Use module created (file robot_return_to_origin.rs)
 mod robot_return_to_origin;
 
 fn main() {
-    let s = "RLDUUDRLLRUDURLDURLDDRUL".to_string();
+    // Sequence of movements to the robot
+    let s = "RLDULLLLLUUUUULLLLLUUUUURDRDRDRDRDDDDDDRRRRRUDLR".to_string();
+    
+    // Result of the given sequence
     println!("{}", robot_return_to_origin::judge_circle(s));
 }
--- src/robot_return_to_origin.rs
+++ src/robot_return_to_origin.rs
@@ -1,16 +1,23 @@
+// Use the library provided by the discipline to run parallel functionalities
 use diam::join;
 
+// Define the size of the slice to work on, instead of keep dividing
 const MIN_SLICE_LENGTH: usize = 10;
 
+// A struct to deal with coordinates in the cartesian plane
 struct Coord {
     x: i32,
     y: i32,
 }
 
+// Recursive function that returns the displacement vector
 fn par_goes_to(moves: &str) -> Coord {
+    // Final coordinates
     let mut coord = Coord { x: 0, y: 0 };
 
+    // Base case: if the 'slice' is smaller than MIN_SLICE_LENGTH 
     if moves.len() < MIN_SLICE_LENGTH {
+        // Iterate over the chars and increment/decrement the coordinates
         for c in moves.chars() {
             if c == 'R' {
                 coord.x += 1;
@@ -26,6 +33,7 @@ fn par_goes_to(moves: &str) -> Coord {
             }
         }
     }
+    // Recursive case: split the string into two parts and sum the coordinates of its results
     else {
         let (left_coord, right_coord) = join(
             || par_goes_to(&moves[..(moves.len() / 2)]),
@@ -34,11 +42,15 @@ fn par_goes_to(moves: &str) -> Coord {
         coord.x = left_coord.x + right_coord.x;
         coord.y = left_coord.y + right_coord.y;
     }
+    // Return the coordinate of the slice analyzed
     coord
 }
 
+// Function used to be compatible with the LeetCode structure
 pub fn judge_circle(moves: String) -> bool {
+    // Convert the String into &str when calling the par_goes_to function
     let coord = par_goes_to(&moves);
+    // Check whether the final coordinate is the origin or not
     return coord.x == 0 && coord.y == 0;
 }
 
